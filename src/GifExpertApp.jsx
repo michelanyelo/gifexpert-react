543eeb60f45a385499ddace804f229d053d4d130
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-03-06 14:55:27 -0300

    Agregar componente AddCategory y funcionalidad para agregar categorías

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,23 @@
 import {useState} from "react";
+import {AddCategory} from "./components/AddCategory.jsx";
 
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['disney', 'naruto'])
+
+    const onAddCategory = () => {
+        const newCategory = 'One Piece'
+        setCategories([...categories, newCategory])
+    }
 
     return (<>
         {/*titulo*/}
         <h1>Gifly</h1>
 
         {/*busqueda*/}
+        <AddCategory/>
+
+        <button onClick={onAddCategory}>Agregar a la lista</button>
 
         {/*listado de gif*/}
         <ol>
